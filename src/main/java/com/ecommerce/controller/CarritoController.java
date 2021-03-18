70a4aa1be771a297951b80ed4f66676cdbe94bbd
JCMarrey <juanmartinez369@gmail.com>
2021-03-18 00:25:21 -0600

    carrito listo

--- src/main/java/com/ecommerce/controller/CarritoController.java
+++ src/main/java/com/ecommerce/controller/CarritoController.java
@@ -3,6 +3,7 @@ package com.ecommerce.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import com.ecommerce.service.BusquedaService;
 import com.ecommerce.service.CarritoService;
 import com.ecommerce.service.ProductoService;
 
-//esto controlador no se usa aún... todo es a través del controllador de carrito
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping ("/carrito/api")
 public class CarritoController {
@@ -23,26 +24,34 @@ public class CarritoController {
 	@Autowired(required = true)
 	private CarritoService carritoService;
 	
-	@Autowired(required = true)
-	private ProductoService productoService;
 	
 	@Autowired(required = true)
 	private BusquedaService busquedaService;
 	
-	@PostMapping("/addItem/{idCarrito}&{idProducto}")
-	public String addItemToCarrito(@PathVariable (value = "idProducto") Integer idProducto, @PathVariable (value = "idCarrito") Integer idCarrito){
+	@Autowired(required = true)
+	private ProductoService productoService;
+
+	
+	@GetMapping("/addItem/{idCarrito}&{idProducto}")
+	public Producto addItemToCarrito(@PathVariable (value = "idProducto") Integer idProducto, @PathVariable (value = "idCarrito") Integer idCarrito){
 		
 		//buscar el carrito de compras 
 		if(carritoService.agregarProductoCarrito(idCarrito, idProducto) == null) {
-			return "hubo un error al agregar el producto";
+			return null; //mandar mensaje de erroor al agregar
 		}
 		/*buscar id del usuario para poder mandarlo y que este quede registrado en tabla reviews*/
-		
-		
 		int idUsuario = busquedaService.buscarUsuarioCarrito(idCarrito);
 		busquedaService.agregarProductoCarrito(idCarrito, idProducto, idUsuario);
-		System.out.println("EL ID DEL USUARIO ES:..." + idUsuario);
-		return "producto agregado con éxito " + "mostrar carrito"; 
+		
+		//esto sirve para mostrar la información del producto así como en mercado libre.
+		
+		Producto oProducto = productoService.buscarProducto(idProducto);
+		//estoo se puede cambiar por String para mandar una URL y quizás después el objeto JSON:"
+		
+		/*"\"producto agregado..\" + \" los datos del producto son:\" + oProducto.getNombre() + \"\\n\"\r\n"
+		+ "				+oProducto.getDescripcion();	*/
+		System.out.println("producto agregado con éxito...");
+		return oProducto;
 	}
 	
 	//método para visualizar productos del carrito
@@ -53,12 +62,16 @@ public class CarritoController {
 	
 	//método para eliminar un producto del carrito
 	@DeleteMapping("/borrarProductoCarrito/{idCarrito}&{idProducto}")
-	public String borrarProductoCarrito (@PathVariable (value = "idCarrito") int idCarrito, @PathVariable
+	public List <Producto> borrarProductoCarrito (@PathVariable (value = "idCarrito") int idCarrito, @PathVariable
 	(value = "idProducto") int idProducto) {
 		
-	//carritoService.eliminarProductoCarritoById(idCarrito, idProducto);
+		
+	carritoService.eliminarProductoCarritoById(idCarrito, idProducto);	
+	
+	//este método solo modifica la fecha de eliminación de la tabla oculta.
 	busquedaService.agregarFechaProductoEliminado(idCarrito);
-	 	return "producto con id: "+ idProducto + " eliminado .. recargar página con prpductos restantes" ;
+	System.out.println("producto eliminado con éxito... y mostrar los restantes productos");
+	return productoService.misProductosCarrito(idCarrito);
 	}
 
 }
